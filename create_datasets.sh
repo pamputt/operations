f285a5dcb410c905cecbc46e5b6c6a6559ee8939
Lopez Hugo <hugo.lpz@gmail.com>
2018-12-16 13:34:40 +0100

    Refactor: create all zips ; then moving them all.

--- create_datasets.sh
+++ create_datasets.sh
@@ -25,19 +25,22 @@ mkdir -p /tmp/datasets
 echo "--> Force the download of recent changes..."
 /usr/bin/python3.5 /home/www/CommonsDownloadTool/commons_download_tool.py --keep --sparqlurl https://lingualibre.fr/bigdata/namespace/wdq/sparql --sparql "SELECT ?file ?filename WHERE { ${query} FILTER( ?date > \"`date --rfc-3339=date --date="- 2 days"`\"^^xsd:dateTime ). }" --threads 4 --directory /tmp/datasets/raw/ --forcedownload --nozip --fileformat ogg
 
-echo "--> Download and package all sounds..."
-/usr/bin/python3.5 /home/www/CommonsDownloadTool/commons_download_tool.py --keep --sparqlurl https://lingualibre.fr/bigdata/namespace/wdq/sparql --sparql "SELECT ?file ?filename WHERE { ${query} }" --threads 4 --directory /tmp/datasets/raw/ --output "/tmp/datasets/LinguaLibre-ALL.zip" --fileformat ogg
+echo "--> Download and package ALL sounds..."
+/usr/bin/python3.5 /home/www/CommonsDownloadTool/commons_download_tool.py --keep --sparqlurl https://lingualibre.fr/bigdata/namespace/wdq/sparql --sparql "SELECT ?file ?filename WHERE { ${query} }" --threads 4 --directory /tmp/datasets/raw/ --output "/tmp/datasets/ALL.zip" --fileformat ogg
 
-echo "--> Move the global zip in the public area"
-mv /tmp/datasets/LinguaLibre-ALL.zip /home/www/datasets/
-
-echo "--> Packahe each language individually"
+echo "--> Package each language individually"
 for qidpath in /tmp/datasets/raw/*;
 do
   qid=$(echo ${qidpath##*/} | cut -d' ' -f 1)
   if [[ $qid == Q* ]] ; then
     echo "--> Processing ${qid}..."
-    /usr/bin/python3.5 /home/www/CommonsDownloadTool/commons_download_tool.py --keep --sparqlurl https://lingualibre.fr/bigdata/namespace/wdq/sparql --sparql "SELECT ?file ?filename WHERE { ${query} ?record prop:P4 entity:${qid}. }" --threads 4 --directory /tmp/datasets/raw/ --output "/tmp/datasets/${qid}.zip" --fileformat ogg
-    mv "/tmp/datasets/${qid}.zip" "/home/www/datasets/LinguaLibre-${qidpath}.zip"
+    /usr/bin/python3.5 /home/www/CommonsDownloadTool/commons_download_tool.py --keep --sparqlurl https://lingualibre.fr/bigdata/namespace/wdq/sparql --sparql "SELECT ?file ?filename WHERE { ${query} ?record prop:P4 entity:${qid}. }" --threads 4 --directory /tmp/datasets/raw/ --output "/tmp/datasets/${qidpath}.zip" --fileformat ogg
   fi
 done
+
+echo "--> Move zip archives in the public area, ALL and by language"
+for filename in /tmp/datasets/*.zip;
+do 
+  zip_basename="$(basename "$filename")"
+  mv "$filename" "/home/www/datasets/LinguaLibre-$zip_basename";
+done;

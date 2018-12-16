1c5ef61cd13990fba74f53330aad6ff3ed8880bc
Lopez Hugo <hugo.lpz@gmail.com>
2018-12-16 16:53:14 +0100

    folder as {qid}_({iso})_({langLabel eng})

--- create_datasets.sh
+++ create_datasets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-query="?record prop:P2 entity:Q2 ;
+### QUERY STRING, key parts for later use.
+queryCore="?record prop:P2 entity:Q2 ;
           prop:P3 ?file ;
           prop:P5 ?locutor ;
           prop:P7 ?transcription ;
@@ -8,45 +8,49 @@ query="?record prop:P2 entity:Q2 ;
           prop:P4 ?lang.
   ?locutor prop:P11 ?user.
 
-  OPTIONAL{ ?lang prop:P13 ?langiso. }
+  OPTIONAL{ ?lang prop:P13 ?langIso. }
   OPTIONAL { ?record prop:P18 ?qualif. }
 
 
 
   ?locutor rdfs:label ?locutorLabel.
-  ?lang rdfs:label ?langLabel.
+  ?lang rdfs:label ?langLabel FILTER (lang(?langLabel) = 'en')."
+queryBinds="
   BIND( CONCAT(
     SUBSTR(STR(?lang), 31),
-    IF(BOUND(?langiso), CONCAT('_(', ?langiso, ')'), '(no_langiso)'),
-    IF(BOUND(?langLabel), CONCAT('_(', ?langLabel, ')'), '(no_langLabel)')
+    IF(BOUND(?langIso), CONCAT('_(', ?langIso, ')'), '_(no_langIso)'),
+    IF(BOUND(?langLabel), CONCAT('_(', ?langLabel, ')'), '_(no_langLabel)')
   ) AS ?langstr)
   BIND( IF( STR(?locutorLabel) = ?user, '', CONCAT( ' (', ?locutorLabel, ')' ) ) AS ?locutorstr )
   BIND( IF(BOUND(?qualif), CONCAT(' (', ?qualif, ')'), '') AS ?qualifstr )
   BIND(CONCAT(?langstr, '/', ?user, ?locutorstr, '/', ?transcription, ?qualifstr, '.wav') AS ?filename)
 "
-# edit langstr to add english name
+queryFocusPeriod=`date --rfc-3339=date --date="- 2 days"`
+queryFilterRecent="FILTER( ?date > \"$queryFocusPeriod\"^^xsd:dateTime )."
+query="$queryCore $queryBinds"
 
-
-mkdir -p /tmp/datasets
+### DOWNLOAD VIA CommonsDownloadTool & QUERY
+mkdir -p  /tmp/datasets /tmp/datasets/raw/
 
 echo "--> Force the download of recent changes..."
-/usr/bin/python3.5 /home/www/CommonsDownloadTool/commons_download_tool.py --keep --sparqlurl https://lingualibre.fr/bigdata/namespace/wdq/sparql --sparql "SELECT ?file ?filename WHERE { ${query} FILTER( ?date > \"`date --rfc-3339=date --date="- 2 days"`\"^^xsd:dateTime ). }" --threads 4 --directory /tmp/datasets/raw/ --forcedownload --nozip --fileformat ogg
+/usr/bin/python3.6 /home/www/CommonsDownloadTool/commons_download_tool.py --keep --sparqlurl https://lingualibre.fr/bigdata/namespace/wdq/sparql --sparql "SELECT ?file ?filename WHERE { ${query} ${queryFilterRecent} }" --threads 4 --directory  /tmp/datasets/raw/ --forcedownload --nozip --fileformat ogg
 
 echo "--> Download and package ALL sounds..."
-/usr/bin/python3.5 /home/www/CommonsDownloadTool/commons_download_tool.py --keep --sparqlurl https://lingualibre.fr/bigdata/namespace/wdq/sparql --sparql "SELECT ?file ?filename WHERE { ${query} }" --threads 4 --directory /tmp/datasets/raw/ --output "/tmp/datasets/ALL.zip" --fileformat ogg
+/usr/bin/python3.5 /home/www/CommonsDownloadTool/commons_download_tool.py --keep --sparqlurl https://lingualibre.fr/bigdata/namespace/wdq/sparql --sparql "SELECT ?file ?filename WHERE { ${query} }" --threads 4 --directory  /tmp/datasets/raw/ --output "/tmp/datasets/ALL.zip" --fileformat ogg
 
 echo "--> Package each language individually"
-for qidpath in /tmp/datasets/raw/*;
+for qidpath in  /tmp/datasets/raw/*;
 do
-  qid=$(echo ${qidpath##*/} | cut -d' ' -f 1)
+  qid=$(echo ${qidpath##*/} | cut -d'_' -f 1)
   if [[ $qid == Q* ]] ; then
     echo "--> Processing ${qid}..."
-    /usr/bin/python3.5 /home/www/CommonsDownloadTool/commons_download_tool.py --keep --sparqlurl https://lingualibre.fr/bigdata/namespace/wdq/sparql --sparql "SELECT ?file ?filename WHERE { ${query} ?record prop:P4 entity:${qid}. }" --threads 4 --directory /tmp/datasets/raw/ --output "/tmp/datasets/${qidpath}.zip" --fileformat ogg
+    /usr/bin/python3.5 /home/www/CommonsDownloadTool/commons_download_tool.py --keep --sparqlurl https://lingualibre.fr/bigdata/namespace/wdq/sparql --sparql "SELECT ?file ?filename WHERE { ${query} ?record prop:P4 entity:${qid}. }" --threads 4 --directory  /tmp/datasets/raw/ --output "/tmp/datasets/${qidpath}.zip" --fileformat ogg
   fi
 done
 
+### MOVE TO PUBLIC FOLDER
 echo "--> Move zip archives in the public area, ALL and by language"
-for filename in /tmp/datasets/*.zip;
+for filename in  /tmp/datasets/*.zip;
 do
   zip_basename="$(basename "$filename")"
   mv "$filename" "/home/www/datasets/LinguaLibre-$zip_basename";

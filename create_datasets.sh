f37bd987665375b52c89398102854c20d7a39334
0x010C <gh@0x010c.fr>
2018-12-18 00:27:39 +0100

    Review: performance, comment, fix small issues
    line 27-28: keep it simple, no need of 2 separate variable if their only goal is to concatenate them right after
    line 30: also, no need to shout
    line 52: ${qidpath} contain the complete path to the target folder, we have to extract it's name to use it
    line 57: No need of a loop here

--- create_datasets.sh
+++ create_datasets.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-### QUERY STRING, key parts for later use.
-queryCore="?record prop:P2 entity:Q2 ;
+
+### Sparql query.
+query="?record prop:P2 entity:Q2 ;
           prop:P3 ?file ;
           prop:P5 ?locutor ;
           prop:P7 ?transcription ;
@@ -12,44 +13,46 @@ queryCore="?record prop:P2 entity:Q2 ;
   OPTIONAL { ?record prop:P18 ?qualif. }
 
   ?locutor rdfs:label ?locutorLabel.
-  ?lang rdfs:label ?langLabel FILTER (lang(?langLabel) = 'en')."
-queryBinds="
+  ?lang rdfs:label ?langLabel FILTER (lang(?langLabel) = 'en').
+
   BIND( CONCAT(
     SUBSTR(STR(?lang), 31),
-    IF(BOUND(?langIso), CONCAT('_(', ?langIso, ')'), '_(no_langIso)'),
-    IF(BOUND(?langLabel), CONCAT('_(', ?langLabel, ')'), '_(no_langLabel)')
+    '-',
+    IF(BOUND(?langIso), ?langIso, 'mis'),
+    IF(BOUND(?langLabel), CONCAT('-', ?langLabel), '')
   ) AS ?langstr)
   BIND( IF( STR(?locutorLabel) = ?user, '', CONCAT( ' (', ?locutorLabel, ')' ) ) AS ?locutorstr )
   BIND( IF(BOUND(?qualif), CONCAT(' (', ?qualif, ')'), '') AS ?qualifstr )
   BIND(CONCAT(?langstr, '/', ?user, ?locutorstr, '/', ?transcription, ?qualifstr, '.wav') AS ?filename)
 "
-queryFocusPeriod=`date --rfc-3339=date --date="- 2 days"`
-queryFilterRecent="FILTER( ?date > \"$queryFocusPeriod\"^^xsd:dateTime )."
-query="$queryCore $queryBinds"
+queryFilterRecent="FILTER( ?date > \"`date --rfc-3339=date --date=\"- 5 days\"`\"^^xsd:dateTime )."
 
-### DOWNLOAD VIA CommonsDownloadTool & QUERY
+# Create (if it doesn't exist already) a temp folder for all the records we will download
 mkdir -p  /tmp/datasets /tmp/datasets/raw/
 
+# Download all recently (> 2 days) recorded files
+# We --forcedownload  the recently uploaded files only,
+# to get the newer version of those that have been updated since last run
 echo "--> Force the download of recent changes..."
 /usr/bin/python3.5 /home/www/CommonsDownloadTool/commons_download_tool.py --keep --sparqlurl https://lingualibre.fr/bigdata/namespace/wdq/sparql --sparql "SELECT ?file ?filename WHERE { ${query} ${queryFilterRecent} }" --threads 4 --directory  /tmp/datasets/raw/ --forcedownload --nozip --fileformat ogg
 
+# Check that we have all the files (or download them) and zip everything
+# We could use zip directly (as the previous command also ensure we got every new files),
+# but by doing it this way deleted files on Lingua Libre won't be shipped even if cached.
 echo "--> Download and package ALL sounds..."
-/usr/bin/python3.5 /home/www/CommonsDownloadTool/commons_download_tool.py --keep --sparqlurl https://lingualibre.fr/bigdata/namespace/wdq/sparql --sparql "SELECT ?file ?filename WHERE { ${query} }" --threads 4 --directory  /tmp/datasets/raw/ --output "/tmp/datasets/ALL.zip" --fileformat ogg
+/usr/bin/python3.5 /home/www/CommonsDownloadTool/commons_download_tool.py --keep --sparqlurl https://lingualibre.fr/bigdata/namespace/wdq/sparql --sparql "SELECT ?file ?filename WHERE { ${query} }" --threads 4 --directory  /tmp/datasets/raw/ --output "/tmp/datasets/lingualibre_full.zip" --fileformat ogg
 
+# Package each language individually
 echo "--> Package each language individually"
 for qidpath in  /tmp/datasets/raw/*;
 do
-  qid=$(echo ${qidpath##*/} | cut -d'_' -f 1)
+  qid=$(echo ${qidpath##*/} | cut -d'-' -f 1)
   if [[ $qid == Q* ]] ; then
     echo "--> Processing ${qid}..."
-    /usr/bin/python3.5 /home/www/CommonsDownloadTool/commons_download_tool.py --keep --sparqlurl https://lingualibre.fr/bigdata/namespace/wdq/sparql --sparql "SELECT ?file ?filename WHERE { ${query} ?record prop:P4 entity:${qid}. }" --threads 4 --directory  /tmp/datasets/raw/ --output "/tmp/datasets/${qidpath}.zip" --fileformat ogg
+    /usr/bin/python3.5 /home/www/CommonsDownloadTool/commons_download_tool.py --keep --sparqlurl https://lingualibre.fr/bigdata/namespace/wdq/sparql --sparql "SELECT ?file ?filename WHERE { ${query} ?record prop:P4 entity:${qid}. }" --threads 4 --directory  /tmp/datasets/raw/ --output "/tmp/datasets/${qidpath##*/}.zip" --fileformat ogg
   fi
 done
 
 ### MOVE TO PUBLIC FOLDER
-echo "--> Move zip archives in the public area, ALL and by language"
-for filename in  /tmp/datasets/*.zip;
-do
-  zip_basename="$(basename "$filename")"
-  mv "$filename" "/home/www/datasets/LinguaLibre-$zip_basename";
-done;
+echo "--> Move all zip archives in the public datasets folder"
+mv /tmp/datasets/*.zip /home/www/datasets/
